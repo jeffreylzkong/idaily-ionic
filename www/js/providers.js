7f489d4df7b3a8875d3ae32af7e1ff3b054ef33c
Jeffrey Kong <jeffreykong@fortinet.com>
2015-03-06 15:23:58 -0800

    Fix title encoding

--- www/js/providers.js
+++ www/js/providers.js
@@ -58,7 +58,7 @@ angular.module('idaily.providers', [])
       var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
         commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
       return input.replace(commentsAndPhpTags, '')
-        .replace('&#39;', "'")
+        .replace(/&nbsp/g, "'").replace(/&#39;/g, "'")
         .replace(tags, function($0, $1) {
           return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
         });
